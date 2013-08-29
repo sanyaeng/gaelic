1d2bb2d5c3595c719b2078d6f0370e902f0fdddd
se <se@goldengekko.com>
2013-08-29 15:16:58 +0700

    New service need to be filter on email, not on the user name

--- gaelic-oauth2-provider/src/main/java/com/wadpam/gaelic/oauth/provider/service/ProfileService.java
+++ gaelic-oauth2-provider/src/main/java/com/wadpam/gaelic/oauth/provider/service/ProfileService.java
@@ -12,6 +12,7 @@ import com.wadpam.gaelic.oauth.provider.domain.Do2pProfile;
 
 /**
  * Encrypts the password when Creating or Updating
+ * 
  * @author sosandstrom
  */
 public class ProfileService extends MardaoCrudService<Do2pProfile, Long, Do2pProfileDao> {
@@ -19,13 +20,13 @@ public class ProfileService extends MardaoCrudService<Do2pProfile, Long, Do2pPro
     public ProfileService() {
         super(Do2pProfile.class, Long.class, Do2pProfileDaoBean.class);
     }
-    
+
     protected void checkUsername(String username, Long currentUserId) {
         if (null == username) {
             throw new ConflictException(ProviderService.ERR_USERNAME_REQUIRED, "Username mandatory", null);
         }
         Long key = dao.findKeyByUsername(username);
-        
+
         // existing user, not same as this being changed
         if (null != key && !key.equals(currentUserId)) {
             throw new ConflictException(ProviderService.ERR_USERNAME_CONFLICT, username, null);
@@ -34,40 +35,50 @@ public class ProfileService extends MardaoCrudService<Do2pProfile, Long, Do2pPro
 
     @Override
     public String create(Do2pProfile domain) {
-        
+
         checkUsername(domain.getUsername(), domain.getId());
-        
+
+        // make sure ID is generated:
+        // super.create(domain);
+        return createProfile(domain);
+    }
+
+    /**
+     * There is some filter which need to be applied for other fields. If the exended class call super.create, it will always
+     * checks the user name only
+     * 
+     * @param domain
+     */
+    protected String createProfile(Do2pProfile domain) {
         // make sure ID is generated:
         super.create(domain);
-        
         // now, encrypt the password (no need to patch-get):
         return update(domain, false);
     }
-    
+
     @Override
     public String update(Do2pProfile domain) {
         return update(domain, true);
     }
-    
+
     public String update(Do2pProfile domain, boolean patch) {
         if (patch) {
             checkUsername(domain.getUsername(), domain.getId());
             final Do2pProfile existing = dao.findByPrimaryKey(domain.getId());
-            
+
             // patch properties
             if (null != existing.getSecret() && null == domain.getSecret()) {
                 domain.setSecret(existing.getSecret());
             }
         }
-        
+
         // encrypt any password not already sized 40 chars
         if (null != domain.getSecret() && 40 != domain.getSecret().length()) {
             final String hash = ProviderService.encryptPassword(domain.getSecret(), domain.getId());
             domain.setSecret(hash);
         }
-        
+
         return super.update(domain);
     }
 
-    
 }
